46393d7e1b8d1f4eb9ac2961fdf96abd7bbd82cd
Zak-Temperton <zbtemperton@gmail.com>
2023-12-20 12:13:54 +1300

    messy but fast

--- src/day18.zig
+++ src/day18.zig
@@ -30,95 +30,6 @@ fn readInt(comptime T: type, input: []const u8, i: *usize) T {
     return res;
 }
 
-fn part1(alloc: Allocator, input: []const u8) !usize {
-    var i: usize = 0;
-    var x: isize = 0;
-    var y: isize = 0;
-
-    var shape = std.AutoHashMap(isize, std.ArrayList(isize)).init(alloc);
-    errdefer {
-        var iterator = shape.valueIterator();
-        while (iterator.next()) |row| row.deinit();
-    }
-    defer shape.deinit();
-    try shape.put(0, blk: {
-        var row = std.ArrayList(isize).init(alloc);
-        try row.append(0);
-        break :blk row;
-    });
-
-    var last_dir: u8 = 'D';
-    while (i < input.len) {
-        const dir = input[i];
-        i += 2;
-        const dist = readInt(isize, input, &i);
-
-        switch (dir) {
-            'L' => {
-                if (last_dir == 'D') {
-                    var row = try shape.getOrPut(y);
-                    if (!row.found_existing) {
-                        row.value_ptr.* = std.ArrayList(isize).init(alloc);
-                    }
-                    try row.value_ptr.*.append(x);
-                }
-                x -= dist;
-            },
-            'R' => {
-                if (last_dir == 'U') {
-                    var row = try shape.getOrPut(y);
-                    if (!row.found_existing) {
-                        row.value_ptr.* = std.ArrayList(isize).init(alloc);
-                    }
-                    try row.value_ptr.*.append(x);
-                }
-                x += dist;
-            },
-            'U' => {
-                var target = y - dist;
-                if (last_dir == 'R') y -= 1;
-                while (y > target) : (y -= 1) {
-                    var row = try shape.getOrPut(y);
-                    if (!row.found_existing) {
-                        row.value_ptr.* = std.ArrayList(isize).init(alloc);
-                    }
-                    try row.value_ptr.*.append(x);
-                }
-            },
-            'D' => {
-                var target = y + dist;
-                if (last_dir == 'L') y += 1;
-                while (y < target) : (y += 1) {
-                    var row = try shape.getOrPut(y);
-                    if (!row.found_existing) {
-                        row.value_ptr.* = std.ArrayList(isize).init(alloc);
-                    }
-                    try row.value_ptr.*.append(x);
-                }
-            },
-            else => unreachable,
-        }
-        last_dir = dir;
-        nextLine(input, &i);
-    }
-    var area: usize = 0;
-    var iterator = shape.iterator();
-    while (iterator.next()) |row| {
-        std.mem.sortUnstable(isize, row.value_ptr.items, {}, std.sort.asc(isize));
-
-        var j: usize = 0;
-        while (j < row.value_ptr.items.len - 1) : (j += 2) {
-            if (j > 2 and row.value_ptr.items[j - 1] == row.value_ptr.items[j]) {
-                area += @intCast(row.value_ptr.items[j + 1] - row.value_ptr.items[j]);
-            } else {
-                area += @intCast(row.value_ptr.items[j + 1] - row.value_ptr.items[j] + 1);
-            }
-        }
-        row.value_ptr.deinit();
-    }
-    return area;
-}
-
 fn readHex(comptime T: type, input: []const u8, i: *usize) T {
     var res: T = 0;
     const len = i.* + 5;
@@ -132,95 +43,400 @@ fn readHex(comptime T: type, input: []const u8, i: *usize) T {
     return res;
 }
 
-fn part2(alloc: Allocator, input: []const u8) !usize {
+const Shape = struct {
+    const Row = struct {
+        const RowTreap = std.Treap([2]isize, Row.order);
+        const RowNode = RowTreap.Node;
+
+        y: isize,
+        treap: RowTreap,
+
+        fn order(a: [2]isize, b: [2]isize) std.math.Order {
+            return std.math.order(a[0], b[0]);
+        }
+
+        fn put(self: *Row, alloc: Allocator, x: isize, dist: isize) !void {
+            var entry = self.treap.getEntryFor(.{ x, dist });
+            if (entry.node) |n| {
+                entry.set(null);
+                alloc.destroy(n);
+            } else {
+                var node = try alloc.create(RowNode);
+                entry.set(node);
+            }
+        }
+
+        fn deinit(self: *Row, alloc: Allocator) void {
+            if (self.treap.root) |root| deinitTreap(alloc, root);
+        }
+    };
+
+    const ShapeTreap = std.Treap(isize, std.math.order);
+    const ShapeNode = ShapeTreap.Node;
+
+    rows: ShapeTreap,
+    map: std.AutoHashMapUnmanaged(isize, Row),
+    allocator: Allocator,
+
+    fn init(alloc: Allocator) Shape {
+        return .{
+            .rows = .{},
+            .map = .{},
+            .allocator = alloc,
+        };
+    }
+
+    fn put(self: *Shape, y: isize, x: isize, dist: isize) !void {
+        var entry = self.rows.getEntryFor(y);
+        if (entry.node) |node| {
+            try self.map.getPtr(node.key).?.put(self.allocator, x, dist);
+        } else {
+            var node = try self.allocator.create(ShapeNode);
+            entry.set(node);
+            try self.map.put(self.allocator, y, blk: {
+                var row = Row{ .y = y, .treap = .{} };
+                try row.put(self.allocator, x, dist);
+                break :blk row;
+            });
+        }
+    }
+
+    fn minHeight(list: *std.ArrayList([2]isize), limit: isize) !isize {
+        var min: isize = std.math.maxInt(isize);
+        for (list.items) |n| {
+            if (n[1] < min) min = n[1];
+        }
+        if (min >= limit) {
+            min = limit;
+        }
+        for (list.items) |*n| {
+            n[1] -= min;
+        }
+        return min;
+    }
+
+    fn asc(context: void, l: [2]isize, r: [2]isize) bool {
+        _ = context;
+        return l[0] < r[0];
+    }
+
+    fn area(self: *Shape) !isize {
+        var sum: isize = 0;
+        var iter = self.rows.inorderIterator();
+
+        var curr = std.ArrayList([2]isize).init(self.allocator);
+        defer curr.deinit();
+
+        var y: isize = 0;
+        if (iter.next()) |first| {
+            y = first.key;
+            var row = self.map.getPtr(first.key).?;
+            var row_iter = row.treap.inorderIterator();
+            while (row_iter.next()) |next| try curr.append(next.key);
+        }
+        while (iter.next()) |next| {
+            var diff = next.key - y;
+            y = next.key;
+            var min = try minHeight(&curr, diff);
+            while (diff > 0 and curr.items.len > 0) {
+                std.debug.assert(curr.items.len & 1 == 0);
+                var block: isize = 0;
+                var i: usize = 0;
+                while (i < curr.items.len - 1) : (i += 2) {
+                    block += curr.items[i + 1][0] - curr.items[i][0] + 1;
+                }
+                sum += block * (min);
+
+                diff -= min;
+
+                var new_list = try std.ArrayList([2]isize).initCapacity(self.allocator, curr.items.len);
+                for (curr.items) |item| {
+                    if (item[1] > 0) new_list.appendAssumeCapacity(item);
+                }
+                curr.deinit();
+                curr = new_list;
+
+                std.mem.sortUnstable([2]isize, curr.items, {}, asc);
+                min = try minHeight(&curr, diff);
+            }
+
+            var new_list = try std.ArrayList([2]isize).initCapacity(self.allocator, curr.items.len);
+            for (curr.items) |item| {
+                if (item[1] > 0) new_list.appendAssumeCapacity(item);
+            }
+            curr.deinit();
+            curr = new_list;
+
+            var row = self.map.getPtr(next.key).?;
+            var row_iter = row.treap.inorderIterator();
+            while (row_iter.next()) |col| {
+                try curr.append(col.key);
+            }
+            std.mem.sortUnstable([2]isize, curr.items, {}, asc);
+        }
+
+        var i: usize = 0;
+        while (i < curr.items.len - 1) : (i += 2) {
+            std.debug.assert(curr.items[i + 1][1] == curr.items[i][1]);
+            sum += (curr.items[i + 1][0] - curr.items[i][0] + 1) * curr.items[i][1];
+        }
+
+        return sum;
+    }
+
+    fn deinit(self: *Shape) void {
+        if (self.rows.root) |root| deinitTreap(self.allocator, root);
+        var map_iter = self.map.valueIterator();
+        while (map_iter.next()) |next| next.deinit(self.allocator);
+        self.map.deinit(self.allocator);
+    }
+};
+
+fn deinitTreap(alloc: Allocator, root: anytype) void {
+    for (root.children) |child| {
+        if (child) |c| deinitTreap(alloc, c);
+    }
+    alloc.destroy(root);
+}
+fn part1(alloc: Allocator, input: []const u8) !isize {
     var i: usize = 0;
     var x: isize = 0;
     var y: isize = 0;
 
-    var shape = std.AutoHashMap(isize, std.ArrayList(isize)).init(alloc);
-    errdefer {
-        var iterator = shape.valueIterator();
-        while (iterator.next()) |row| row.deinit();
-    }
+    var shape = Shape.init(alloc);
     defer shape.deinit();
-    try shape.put(0, blk: {
-        var row = std.ArrayList(isize).init(alloc);
-        try row.append(0);
-        break :blk row;
-    });
 
-    var last_dir: u8 = 'D';
+    const first_dir = input[i];
+    i += 2;
+    const first_dist = readInt(isize, input, &i);
+    nextLine(input, &i);
+
+    var last_dir: u8 = first_dir;
+    var last_dist: isize = first_dist;
+
+    switch (first_dir) {
+        'L' => {
+            x -= first_dist;
+        },
+        'R' => {
+            x += first_dist;
+        },
+        else => unreachable,
+    }
+    while (i < input.len) {
+        const dir = input[i];
+        i += 2;
+        const dist = readInt(isize, input, &i);
+        nextLine(input, &i);
+
+        switch (dir) {
+            'L' => {
+                if (last_dir == 'U') {
+                    if (last_dist > 1)
+                        try shape.put(y + 1, x, last_dist - 1);
+                } else {
+                    if (last_dist > 0)
+                        try shape.put(y + 1 - last_dist, x, last_dist);
+                }
+                x -= dist;
+            },
+            'R' => {
+                if (last_dir == 'U') {
+                    if (last_dist > 0)
+                        try shape.put(y, x, last_dist);
+                } else {
+                    if (last_dist > 1)
+                        try shape.put(y + 1 - last_dist, x, last_dist - 1);
+                }
+                x += dist;
+            },
+            'U' => {
+                y -= dist;
+                if (last_dir == 'R') {
+                    last_dist = dist;
+                } else {
+                    last_dist = dist + 1;
+                }
+            },
+            'D' => {
+                if (last_dir == 'R') {
+                    last_dist = dist + 1;
+                } else {
+                    last_dist = dist;
+                }
+                y += dist;
+            },
+            else => unreachable,
+        }
+        last_dir = dir;
+    }
+    {
+        const dir = first_dir;
+        const dist = first_dist;
+
+        switch (dir) {
+            'L' => {
+                if (last_dir == 'U') {
+                    if (last_dist > 1)
+                        try shape.put(y + 1, x, last_dist - 1);
+                } else {
+                    if (last_dist > 0)
+                        try shape.put(y + 1 - last_dist, x, last_dist);
+                }
+                x -= dist;
+            },
+            'R' => {
+                if (last_dir == 'U') {
+                    if (last_dist > 0)
+                        try shape.put(y, x, last_dist);
+                } else {
+                    if (last_dist > 1)
+                        try shape.put(y + 1 - last_dist, x, last_dist - 1);
+                }
+                x += dist;
+            },
+            'U' => {
+                y -= dist;
+                if (last_dir == 'R') {
+                    last_dist = dist;
+                } else {
+                    last_dist = dist + 1;
+                }
+            },
+            'D' => {
+                if (last_dir == 'R') {
+                    last_dist = dist + 1;
+                } else {
+                    last_dist = dist;
+                }
+                y += dist;
+            },
+            else => unreachable,
+        }
+    }
+    return try shape.area();
+}
+fn part2(alloc: Allocator, input: []const u8) !isize {
+    var i: usize = 0;
+    var x: isize = 0;
+    var y: isize = 0;
+
+    var shape = Shape.init(alloc);
+
+    defer shape.deinit();
+    i += 3;
+    while (input[i] != ' ') i += 1;
+    i += 3;
+    const first_dist = readHex(isize, input, &i);
+    var first_dir = input[i] - '0';
+    nextLine(input, &i);
+
+    var last_dir: u8 = first_dir;
+    var last_dist: isize = first_dist;
+
+    switch (first_dir) {
+        2 => {
+            x -= first_dist;
+        },
+        0 => {
+            x += first_dist;
+        },
+        else => unreachable,
+    }
     while (i < input.len) {
         i += 3;
         while (input[i] != ' ') i += 1;
         i += 3;
         const dist = readHex(isize, input, &i);
         var dir = input[i] - '0';
+        nextLine(input, &i);
 
         switch (dir) {
             2 => {
-                if (last_dir == 1) {
-                    var row = try shape.getOrPut(y);
-                    if (!row.found_existing) {
-                        row.value_ptr.* = std.ArrayList(isize).init(alloc);
-                    }
-                    try row.value_ptr.*.append(x);
+                if (last_dir == 3) {
+                    if (last_dist > 1)
+                        try shape.put(y + 1, x, last_dist - 1);
+                } else {
+                    if (last_dist > 0)
+                        try shape.put(y + 1 - last_dist, x, last_dist);
                 }
                 x -= dist;
             },
             0 => {
                 if (last_dir == 3) {
-                    var row = try shape.getOrPut(y);
-                    if (!row.found_existing) {
-                        row.value_ptr.* = std.ArrayList(isize).init(alloc);
-                    }
-                    try row.value_ptr.*.append(x);
+                    if (last_dist > 0)
+                        try shape.put(y, x, last_dist);
+                } else {
+                    if (last_dist > 1)
+                        try shape.put(y + 1 - last_dist, x, last_dist - 1);
                 }
                 x += dist;
             },
             3 => {
-                var target = y - dist;
-                if (last_dir == 0) y -= 1;
-                while (y > target) : (y -= 1) {
-                    var row = try shape.getOrPut(y);
-                    if (!row.found_existing) {
-                        row.value_ptr.* = std.ArrayList(isize).init(alloc);
-                    }
-                    try row.value_ptr.*.append(x);
+                y -= dist;
+                if (last_dir == 0) {
+                    last_dist = dist;
+                } else {
+                    last_dist = dist + 1;
                 }
             },
             1 => {
-                var target = y + dist;
-                if (last_dir == 2) y += 1;
-                while (y < target) : (y += 1) {
-                    var row = try shape.getOrPut(y);
-                    if (!row.found_existing) {
-                        row.value_ptr.* = std.ArrayList(isize).init(alloc);
-                    }
-                    try row.value_ptr.*.append(x);
+                if (last_dir == 0) {
+                    last_dist = dist + 1;
+                } else {
+                    last_dist = dist;
                 }
+                y += dist;
             },
             else => unreachable,
         }
         last_dir = dir;
-        nextLine(input, &i);
     }
-    var area: usize = 0;
-    var iterator = shape.iterator();
-    while (iterator.next()) |row| {
-        std.mem.sortUnstable(isize, row.value_ptr.items, {}, std.sort.asc(isize));
+    {
+        const dir = first_dir;
+        const dist = first_dist;
 
-        var j: usize = 0;
-        while (j < row.value_ptr.items.len - 1) : (j += 2) {
-            if (j > 2 and row.value_ptr.items[j - 1] == row.value_ptr.items[j]) {
-                area += @intCast(row.value_ptr.items[j + 1] - row.value_ptr.items[j]);
-            } else {
-                area += @intCast(row.value_ptr.items[j + 1] - row.value_ptr.items[j] + 1);
-            }
+        switch (dir) {
+            2 => {
+                if (last_dir == 3) {
+                    if (last_dist > 1)
+                        try shape.put(y + 1, x, last_dist - 1);
+                } else {
+                    if (last_dist > 0)
+                        try shape.put(y + 1 - last_dist, x, last_dist);
+                }
+                x -= dist;
+            },
+            0 => {
+                if (last_dir == 3) {
+                    if (last_dist > 0)
+                        try shape.put(y, x, last_dist);
+                } else {
+                    if (last_dist > 1)
+                        try shape.put(y + 1 - last_dist, x, last_dist - 1);
+                }
+                x += dist;
+            },
+            3 => {
+                y -= dist;
+                if (last_dir == 0) {
+                    last_dist = dist;
+                } else {
+                    last_dist = dist + 1;
+                }
+            },
+            1 => {
+                if (last_dir == 0) {
+                    last_dist = dist + 1;
+                } else {
+                    last_dist = dist;
+                }
+                y += dist;
+            },
+            else => unreachable,
         }
-        row.value_ptr.deinit();
     }
-    return area;
+    return try shape.area();
 }
 
 test "part1" {
